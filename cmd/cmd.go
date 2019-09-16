046bda1ad279c741904cf58847756b3e129cb153
Aryan <collaborativeroom@gmail.com>
2019-09-16 07:18:06 +0430

    wip

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,16 +2,19 @@ package cmd
 
 import (
 	"flag"
-	"os"
+	"fmt"
+	"io/ioutil"
+	"time"
 )
 
-func Init()  {
-	flag.Var()
-	args := os.Args[1:]
-	
+func Init() {
+	name := flag.String("name", "hello", "migration name")
+	flag.Parse()
+	typ := flag.Arg(0)
 
-	if args[0] != "'" {
-
+	switch typ {
+	case "table":
+		_ = ioutil.WriteFile(fmt.Sprintf("%s_create_table_%s.sql", time.Now().UTC().Format("2006_01_02_15_04_05"), *name), []byte(`create table HELLO();`), 0644)
+		break
 	}
-
 }
